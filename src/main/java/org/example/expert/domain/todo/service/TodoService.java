fbf5b365b6d91ac2053d2460c53f9ed2cabff87e
youngee2 <jr3537@naver.com>
2025-06-25 19:27:27 +0900

    refactor: todo service 수정

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class TodoService {
 
     private final TodoRepository todoRepository;
