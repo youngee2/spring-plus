36ef910053eb93e143345910762107005ffa1f37
ahyoung <jr3537@naver.com>
2025-06-30 16:12:43 +0900

    feat: 할 일을 생성한 유저가 담당자로 등록할 수 있게 구현

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
+            "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
